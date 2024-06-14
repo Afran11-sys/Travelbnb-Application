6ae0c5733a36cca8ce7e98f5d25b49e1847aee8c
ppankajlinkedinppankaj <ppankajplinkedin@gmail.com>
2024-06-14 20:21:00 +0530

    14th june code

---
+++ src/main/java/com/travelbnb/config/JWTRequestFilter.java
@@ -0,0 +1,32 @@
+package com.travelbnb.config;
+
+import com.travelbnb.service.JWTService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JWTRequestFilter extends OncePerRequestFilter {
+    private JWTService jwtService;
+
+    public JWTRequestFilter(JWTService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String tokenHeader = request.getHeader("Authorization");
+        if(tokenHeader != null && tokenHeader.startsWith("Bearer ")){
+            String token = tokenHeader.substring(8, tokenHeader.length() - 1);
+            String username = jwtService.getUserName(token);
+            System.out.println(username);
+        }
+        filterChain.doFilter(request,response);
+    }
+}
